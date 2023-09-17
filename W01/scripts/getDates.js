589e1b9472d4e19a4edde25b7b1ec8cd70a5c2ca
Cabe-Lee <cabetlee@gmail.com>
2023-09-16 23:50:33 -0600

    W01 assignment

---
+++ W01/scripts/getDates.js
@@ -0,0 +1,5 @@
+var currentYear = new Date().getFullYear();
+console.log(currentYear);
+// document.getElementById('copyright').innerHTML = currentYear
+// stuck in getting the funtcions to work.
+let oLastModif = new Date(document.lastModified);
