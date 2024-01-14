985c6f4bc221644a6d65731081c5fe56dcc23046
Cabe-Lee <cabetlee@gmail.com>
2024-01-13 17:09:20 -0700

    Initial

---
+++ scripts/getdate.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+    let year = new Date().getFullYear();
+    const copyText = ("Ⓒ - Cabe - CA, USA");
+
+    document.getElementById("copyright").innerHTML = copyText +' - ' + year;
+
+    const date = new Date(document.lastModified);
+    document.getElementById("lastModified").innerHTML = date;
+}
+
