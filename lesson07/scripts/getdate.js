8b438a173c84deb83ca4e28cbc97c92282d9ea6c
Cabe-Lee <cabetlee@gmail.com>
2024-02-24 14:33:40 -0800

    W07 test 7.

--- lesson07/scripts/getdate.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("year").innerHTML = new Date().getFullYear();
-document.getElementById("date").innerHTML = new Date().toLocaleDateString('en-US');
-document.getElementById("hour").innerHTML = new Date().toLocaleTimeString('eo', { hour12: false });
