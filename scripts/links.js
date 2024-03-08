b611562914f727ec6cc66e0be50a7bdacd6d8728
Cabe-Lee <cabetlee@gmail.com>
2024-03-07 22:28:09 -0800

    W09 test 2.

--- scripts/links.js
+++ scripts/links.js
@@ -1,24 +1,33 @@
 const baseURL = "https://cabe-lee.github.io/wdd230/";
 const linksURL = "https://cabe-lee.github.io/wdd230/data/links.json";
-let activites = document.querySelector('#learningActivities');
+const card = document.querySelector('#learningActivities');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.table(data.lessons);
-    displayLinks(data);
+    console.log(data);
+    displayWeek(data.lessons);
 };
+getLinks();
 
-function displayLinks(weeks) {
-    weeks.forEach((week) => {
-        const activites = document.createElement("p")
-        // Build the h2 content out to show the prophet's full name
-        activites.textContent = `${week.lesson}: ${week.links}`; // fill in the blank
-        // Build the image portrait by setting all the relevant attributes
-        board.setAttribute('alt', `Learning Activities`); // fill in the blank
-        board.setAttribute('loading', 'lazy');
-    
-        // Append the section(card) with the created elements
-        card.appendChild(fullName); //fill in the blank
+
+function displayWeek(lessons) {
+    lessons.forEach((lesson) => {
+        let section = document.createElement("section");
+        let p = document.createElement("p");
+
+        p.textContent = `${lesson.lesson}:`;
+
+        section.appendChild(p);
+
+        card.appendChild(section);
+        // lessons.links.forEach((link) => {
+        //     let a = document.createElement("a");
+
+        //     a.textContent = `${link.title}`;
+        //     a.href = `${link.url}`;
+            
+        //     section.appendChild(a);
+        // });
     });
 };
