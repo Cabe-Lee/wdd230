d47d264af4a1a8c8d78fef40dae7f130ad1b12c4
Cabe-Lee <cabetlee@gmail.com>
2024-03-09 23:00:30 -0800

    W09.

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -15,33 +15,23 @@ function displayMembers(members) {
         let section = document.createElement("section");
         
         let image = document.createElement("img")
-        let name = document.createElement("h3");
         let address = document.createElement("p");
         let number = document.createElement("p");
         let site = document.createElement("a");
-        let membership = document.createElement("p");
-        let stars = document.createElement("p");
 
         image.setAttribute('src', member.image);
-        image.setAttribute('alt', `image`); // fill in the blank
         image.setAttribute('loading', 'lazy');
         image.setAttribute('width', '100');
         image.setAttribute('height', '100');
-        name.textContent = `${member.name}`;
         address.textContent = `${member.address}`;
         number.textContent = `${member.number}`;
         site.href = member.url;
         site.textContent = member.title;
-        membership.textContent = `${member.membership}`;
-        stars.textContent = `${member.stars}`;
         
         section.appendChild(image)
-        section.appendChild(name);
         section.appendChild(address);
         section.appendChild(number);
         section.appendChild(site);
-        section.appendChild(membership);
-        section.appendChild(stars);
         card.appendChild(section);
     });
 };
