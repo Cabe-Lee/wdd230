5d183f5ce6443ad8e93e2f8fe1c43e813e1d8ccc
Cabe-Lee <cabetlee@gmail.com>
2024-03-11 13:47:08 -0700

    W09 fix.

--- scripts/links.js
+++ scripts/links.js
@@ -7,6 +7,7 @@ async function getLinks() {
     const data = await response.json();
     console.log(data);
     displayWeek(data.lessons);
+    // displayLinks(data.links);
 };
 getLinks();
 
@@ -15,19 +16,31 @@ function displayWeek(lessons) {
     lessons.forEach((lesson) => {
         let section = document.createElement("section");
         let p = document.createElement("p");
-
+        let a = document.createElement("a");
+        
         p.textContent = `${lesson.lesson}:`;
+        a.href = lesson.url1;
+        a.href = lesson.url2;
+        a.href = lesson.url3;
+        a.textContent = lesson.title1;
+        a.textContent = lesson.title2;
+        a.textContent = lesson.title3;
 
         section.appendChild(p);
-
+        section.appendChild(a);
         card.appendChild(section);
-        // lessons.links.forEach((link) => {
-        //     let a = document.createElement("a");
-
-        //     a.textContent = `${link.title}`;
-        //     a.href = `${link.url}`;
-            
-        //     section.appendChild(a);
-        // });
     });
 };
+
+// function displayLinks(links) {
+//         links.forEach(link => {
+//             let section = document.createElement("section");
+//             let a = document.createElement("a");
+            
+//             a.href = link.url;
+//             a.textContent = link.title;
+
+//             section.appendChild(a);
+//             card.appendChild(section);
+//         });
+// }
