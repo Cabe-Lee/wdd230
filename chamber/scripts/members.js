212c27c4be6f44861941321cd4357ed3ff653a5a
Cabe-Lee <cabetlee@gmail.com>
2024-03-22 09:38:54 -0700

    W09 fixes and putting in the work.

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,37 +1,37 @@
 const baseURL = "https://cabe-lee.github.io/wdd230/";
 const linksURL = "https://cabe-lee.github.io/wdd230/chamber/data/members.json";
-const card = document.querySelector('#members');
+const card = document.querySelector('.members');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data.members);
     displayMembers(data.members);
-    displayMemberHome(data.members);
+    // displayMemberHome(data.members);
 };
 getLinks();
 
 function displayMembers(members) {
     members.forEach((member) => {
-        let section = document.createElement("section");
+        let section = document.querySelector(".member-cards")
         
         let image = document.createElement("img");
-        let address = document.createElement("p");
-        let number = document.createElement("p");
+        let name = document.createElement("h4");
+        let membership = document.createElement("p");
         let site = document.createElement("a");
 
         image.setAttribute('src', member.image);
         image.setAttribute('loading', 'lazy');
         image.setAttribute('width', '100');
         image.setAttribute('height', '100');
-        address.textContent = `${member.address}`;
-        number.textContent = `${member.number}`;
+        name.textContent = member.name;
+        membership.textContent = member.membership;
         site.href = member.url;
         site.textContent = member.title;
         
         section.appendChild(image);
-        section.appendChild(address);
-        section.appendChild(number);
+        section.appendChild(name);
+        section.appendChild(membership);
         section.appendChild(site);
         card.appendChild(section);
     });
@@ -40,13 +40,13 @@ function displayMembers(members) {
 
 
 let memberCard1 = document.querySelector("#memberCard1");
-let title1 = document.querySelector("#title1");
 let logo1 = document.querySelector("#logo1");
+let title1 = document.querySelector("#title1");
 let membership1 = document.querySelector("#membership1");
 
 let memberCard2 = document.querySelector("#memberCard2");
-let title2 = document.querySelector("#title2");
 let logo2 = document.querySelector("#logo2");
+let title2 = document.querySelector("#title2");
 let membership2 = document.querySelector("#membership2");
 
 let memberCard3 = document.querySelector("#memberCard3");
@@ -60,7 +60,7 @@ let membership3 = document.querySelector("#membership3");
 
 
 
-function displayMemberHome(members) {
-    title1.textContent = members[0].name;
+// function displayMemberHome(members) {
+//     title1.textContent = members[0].name;
 
-}
+// }
