954958f354f73219d4c06242804e2547a28eddef
Cabe-Lee <cabetlee@gmail.com>
2024-04-02 14:39:25 -0700

    W09 fixed.

--- lesson12/final-project/scripts/rental-data.js
+++ lesson12/final-project/scripts/rental-data.js
@@ -1,5 +1,4 @@
 const linksURL = "data/rental-data.json";
-// const linksURL = "https://cabe-lee.github.io/wdd230/lesson12/final-project/data/rental-data.json";
 
 async function getJson() {
     const response = await fetch(linksURL);
@@ -11,9 +10,9 @@ getJson();
 
 function displayRentals(data) {
     data.forEach(rental => {
-        let tr = querySelector(".tr")
+        let tbody = querySelector(".tbody")
 
-        let name = document.createElement("th");
+        let name = document.createElement("tr");
         // let max = document.querySelector(".max");
         // let halfRes = document.querySelector(".halfRes");
         // let fullRes = document.querySelector(".fullRes");
@@ -21,25 +20,7 @@ function displayRentals(data) {
         // let fullWalkIn = document.querySelector(".fullWalk-In");
         
         name.textContent = rental.name;
-        tr.appendChild(name);
+        tbody.appendChild(name);
     });
         
 };
-
-// Fetch the JSON data (replace 'your-json-file.json' with the actual file path)
-// Try this out when your done with your break.
-// fetch('your-json-file.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         const tableBody = document.querySelector('#employee-table tbody');
-//         data.forEach(employee => {
-//             const row = document.createElement('tr');
-//             row.innerHTML = `
-//                 <td>${employee.id}</td>
-//                 <td>${employee.name}</td>
-//                 <td>${employee.age}</td>
-//             `;
-//             tableBody.appendChild(row);
-//         });
-//     })
-//     .catch(error => console.error('Error fetching data:', error));
--- scripts/links.js
+++ scripts/links.js
@@ -1,57 +1,35 @@
 const baseURL = "https://cabe-lee.github.io/wdd230/";
 const linksURL = "data/links.json";
-// const linksURL = "https://cabe-lee.github.io/data/links.json";
-const card = document.querySelector('#learningActivities');
+
+const card = document.querySelector(".learningActivities");
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    console.log(data);
-    displayWeek(data.lessons);
-};
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  console.log(data);
+  displayWeek(data.lessons);
+}
 getLinks();
 
-// Template to links to display on HTML.
-
-// let once = document.querySelector("#once");
-// let a = document.createElement("a");
-// a.href = "https://google.com";
-// a.textContent = "Google";
-// once.appendChild(a);
-
-
-
 function displayWeek(lessons) {
-    lessons.forEach((week) => {
+  card.innerHTML = "";
+  const ul = document.createElement("ul");
 
-        let p = document.createElement("p");
-        p.textContent = week.lesson;
+  lessons.forEach((week) => {
+    let p = document.createElement("li");
+    p.textContent = week.lesson;
 
-        console.log(week);
-
-        let a = document.createElement("a");
-
-        week.links.forEach((list, index) => {
-            console.log(list);
-
-        })
-            
-        //     a.href = list.url
-        //     a.textContent = list.title;
-
-        //     if (index > 0) {
-        //         let separator = document.createElement('span');
-
-        //         separator.textContent = ' | ';
-        //         separator.appendChild(a);
-        //         weekLesson.appendChild(separator);
-
-        //     }  else {
-        //         weekLesson.appendChild(a);
-        //     }
-
-        //     card.appendChild(p);
-        //     card.appendChild(a);
-        // });
+    week.links.forEach((list) => {
+      let a = document.createElement("a");
+      a.setAttribute("href", list.url);
+      a.innerHTML = list.title;
+      
+      p.appendChild(a);
+      p.append(" | ");
     });
-};
+
+    ul.appendChild(p);
+  });
+  
+  card.appendChild(ul);
+}
